35b767a00d5f5d12b064e47b3cb830f5933e222d
rinshan_ho <lazuli.amaryllis@gmail.com>
2023-05-27 16:35:32 +0900

    DDせずに起動した場合の挙動の追加。続行しますかでいいえが選択された場合の挙動の実装を今度やる

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import hashlib
-# import sys
+import sys
 
-# paths = []
-# if sys.argv[1:] != []:
-#     paths = sys.argv[1:] # DDされた全ファイルをpathsに格納
-
-paths = ['hoge1.txt', 'hoge2.txt', 'hoge3.txt']
+paths = []
+if sys.argv[1:] != []:
+    paths = sys.argv[1:] # DDされた全ファイルをpathsに格納
 
 hashs = []
 
-for i, path in enumerate(paths):
-    with open(path, 'rb') as f:
-        # print(path)
-        f_data = f.read()
-        #sha256
-        hash = hashlib.sha256(f_data).hexdigest()
-        # print('sha256:', hash)
-        hashs.append(hash)
-        if(i==0): continue
-        if(hashs[0] == hashs[i]):
-            print(f'元ファイル: {paths[0]} と検証先ファイル: {paths[i]} は')
-            print('同一ファイルです\n')
-        else:
-            print(f'元ファイル: {paths[0]} と検証先ファイル: {paths[i]} は')
-            print('改ざんされています\n')
+if(paths==[]):
+    print('元ファイルをD&Dしてください')
+    paths.append(input(': '))
+    print('検証したいファイルをD&Dしてください')
+    paths.append(input(': '))
 
+for i, path in enumerate(paths):
+        with open(path, 'rb') as f:
+            f_data = f.read()
+            hash = hashlib.sha256(f_data).hexdigest() # sha256
+            hashs.append(hash)
+            if(i==0): continue
+            if(hashs[0] == hashs[i]):
+                print(f'元ファイル: {paths[0]} と検証先ファイル: {paths[i]} は')
+                print('同一ファイルです\n')
+            else:
+                print(f'元ファイル: {paths[0]} と検証先ファイル: {paths[i]} は')
+                print('改ざんされています\n')
+
+print('続行しますか？(Y/n)')
+sentaku = input(': ')
